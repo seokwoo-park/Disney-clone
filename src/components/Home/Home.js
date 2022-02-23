95761a291d5994b9b6b3be8201790b907b233b96
Seokwoo Park <dev.seokwoopark@gmail.com>
2022-02-23 12:15:49 +0100

    [Add] Viewers to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "./Home.styled";
 import ImgSlider from "./ImgSlider/ImgSlider";
+import Viewers from "./Viewers/Viewers";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   );
 }
---
+++ src/components/Home/Viewers/Viewers.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Container, Wrap } from "./Viewers.styled";
+
+function Viewers() {
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </Wrap>
+    </Container>
+  );
+}
+
+export default Viewers;
